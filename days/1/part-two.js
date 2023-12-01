8df09e5efdd1666fa70dc0c2abe1d000380f4e2c
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-01 10:23:22 +0100

    1: part two

--- days/1/part-two.js
+++ days/1/part-two.js
@@ -1,7 +1,43 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  return input
+    .map((value) => {
+      const { first, last } = extractFirstAndLastDigit(value)
+      return Number(`${first}${last}`)
+    })
+    .reduce((acc, value) => {
+      return acc + value
+    }, 0)
+}
+
+const numberWords = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+}
+
+function getDigit(str) {
+  return numberWords[str] ?? Number.parseInt(str)
+}
+
+function extractFirstAndLastDigit(str) {
+  const regex = new RegExp(`(?=(\\d|${Object.keys(numberWords).join('|')}))`, 'g')
+
+  const matches = [...str.matchAll(regex)]
+  const first = getDigit(matches.at(0)[1])
+  const last = getDigit(matches.at(-1)[1])
+
+  return { first, last }
+}
 
 export {
   getResult,
+  extractFirstAndLastDigit,
 }
--- days/1/part-two.test.js
+++ days/1/part-two.test.js
@@ -5,12 +5,39 @@ import { parseInputString } from './input.js'
 describe('day1 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `
+      two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(281)
+    })
+  })
+
+  describe('#extractFirstAndLastDigit', () => {
+    it('should return array of int', () => {
+      const str = '4nineeightseven2'
+
+      const { first, last } = partTwo.extractFirstAndLastDigit(str)
+
+      expect(first).toBe(4)
+      expect(last).toBe(2)
+    })
+
+    it('should return array of int for `eight33fiveg5oneeightwoh`', () => {
+      const str = 'eight33fiveg5oneeightwoh'
+
+      const { first, last } = partTwo.extractFirstAndLastDigit(str)
+
+      expect(first).toBe(8)
+      expect(last).toBe(2)
     })
   })
 })
