818ed986da0f096f5695aa99fba8efbe34970fab
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-01 10:24:10 +0100

    fix lint in script

--- scripts/get-day-input.js
+++ scripts/get-day-input.js
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 import { writeFileSync } from 'node:fs'
+import { argv, env } from 'node:process'
 import axios from 'axios'
 
 main()
 
 async function main() {
-  const day = process.argv[2]
+  const day = argv[2]
   const inputUrl = `https://adventofcode.com/2023/day/${day}/input`
-  const { data } = await axios.get(inputUrl, { headers: { Cookie: `session=${process.env.SESSION}` } })
+  const { data } = await axios.get(inputUrl, { headers: { Cookie: `session=${env.SESSION}` } })
   writeFileSync(`./days/${day}/input.txt`, data)
 }
