7665b99937cef4ca233658b0aa0417558e10365a
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-02 08:26:57 +0100

    2: part two

--- days/2/part-two.js
+++ days/2/part-two.js
@@ -1,6 +1,32 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  return input
+    .map((line) => {
+      const quantityEachRound = {
+        red: [],
+        green: [],
+        blue: [],
+      }
+      line.game
+        .split(';')
+        .forEach((round) => {
+          round
+            .split(',')
+            .forEach((cube) => {
+              const cubeInfo = cube.trim().split(' ')
+              quantityEachRound[cubeInfo[1]].push(Number.parseInt(cubeInfo[0]))
+            })
+        })
+
+      return Object.keys(quantityEachRound)
+        .map((color) => {
+          return Math.max(...quantityEachRound[color])
+        })
+        .reduce((acc, curr) => acc * curr, 1)
+    })
+    .reduce((acc, curr) => acc + curr, 0)
+}
 
 export {
   getResult,
