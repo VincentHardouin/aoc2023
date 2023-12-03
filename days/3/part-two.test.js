969afa5b23a18a221b31fa7301499b409d98ad1f
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-03 14:38:39 +0100

    3: part two

--- days/3/part-two.test.js
+++ days/3/part-two.test.js
@@ -23,4 +23,55 @@ describe('day3 | part-two', () => {
       expect(result).toBe(467835)
     })
   })
+
+  describe('#findAdjacentNumberInLine', () => {
+    it('should return adjacent numbers', () => {
+      const inputString = `
+      467..114..`
+      const input = parseInputString(inputString)
+      const result = partTwo.findAdjacentNumberInLine(input[0], { startIndex: 2, endIndex: 4 })
+
+      expect(result).toEqual([467])
+    })
+  })
+
+  describe('#findAdjacentNumbers', () => {
+    it('should return adjacent numbers', () => {
+      const inputString = `
+      467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+      const input = parseInputString(inputString)
+      const result = partTwo.findAdjacentNumbers({ inputIndex: 1, lineIndex: 3 }, input)
+
+      expect(result).toEqual([467, 35])
+    })
+
+    it('right case: should return adjacent numbers', () => {
+      const inputString = `
+      467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+      const input = parseInputString(inputString)
+      const result = partTwo.findAdjacentNumbers({ inputIndex: 0, lineIndex: 4 }, input)
+
+      expect(result).toEqual([114])
+    })
+  })
 })
