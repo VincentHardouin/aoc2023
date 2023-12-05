14e886d2a4e0dc7a7dbb4854aa660aebc815de7f
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-05 09:38:53 +0100

    5: part two not working with input

--- days/5/part-two.js
+++ days/5/part-two.js
@@ -1,6 +1,35 @@
+import _ from 'lodash'
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const seedsWithRange = _.chunk(input.seeds, 2)
+
+  const location = seedsWithRange
+    .map(([seed, range]) => _.range(seed, seed + range))
+    .map((seeds) => {
+      const location = seeds
+        .map(seed => convertToDestination(seed, input.seedToSoil))
+        .map(seed => convertToDestination(seed, input.soilToFertilizer))
+        .map(seed => convertToDestination(seed, input.fertilizerToWater))
+        .map(seed => convertToDestination(seed, input.waterToLight))
+        .map(seed => convertToDestination(seed, input.lightToTemperature))
+        .map(seed => convertToDestination(seed, input.temperatureToHumidity))
+        .map(seed => convertToDestination(seed, input.humidityToLocation))
+      return Math.min(...location)
+    })
+  return Math.min(...location)
+}
+
+function convertToDestination(seed, destinationBlock) {
+  const destination = destinationBlock.find(([_, source, range]) => {
+    return seed >= source && seed <= source + range
+  })
+
+  if (destination)
+    return seed - destination[1] + destination[0]
+
+  return seed
+}
 
 export {
   getResult,
