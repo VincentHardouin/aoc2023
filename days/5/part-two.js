1a2ec967ac28df257deb746a54a2d7326953aa19
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-05 14:29:29 +0100

    5: part two

--- days/5/part-two.js
+++ days/5/part-two.js
@@ -5,30 +5,51 @@ function getResult(input = getInput()) {
   const seedsWithRange = _.chunk(input.seeds, 2)
 
   const location = seedsWithRange
-    .map(([seed, range]) => _.range(seed, seed + range))
-    .map((seeds) => {
-      const location = seeds
-        .map(seed => convertToDestination(seed, input.seedToSoil))
-        .map(seed => convertToDestination(seed, input.soilToFertilizer))
-        .map(seed => convertToDestination(seed, input.fertilizerToWater))
-        .map(seed => convertToDestination(seed, input.waterToLight))
-        .map(seed => convertToDestination(seed, input.lightToTemperature))
-        .map(seed => convertToDestination(seed, input.temperatureToHumidity))
-        .map(seed => convertToDestination(seed, input.humidityToLocation))
-      return Math.min(...location)
+    .map(([seed, range]) => {
+      return {
+        start: seed,
+        end: seed + range,
+      }
     })
-  return Math.min(...location)
+    .flatMap(seed => convertToDestination(seed, input.seedToSoil))
+    .flatMap(seed => convertToDestination(seed, input.soilToFertilizer))
+    .flatMap(seed => convertToDestination(seed, input.fertilizerToWater))
+    .flatMap(seed => convertToDestination(seed, input.waterToLight))
+    .flatMap(seed => convertToDestination(seed, input.lightToTemperature))
+    .flatMap(seed => convertToDestination(seed, input.temperatureToHumidity))
+    .flatMap(seed => convertToDestination(seed, input.humidityToLocation))
+  return Math.min(...location.map(({ start }) => start))
 }
 
-function convertToDestination(seed, destinationBlock) {
-  const destination = destinationBlock.find(([_, source, range]) => {
-    return seed >= source && seed <= source + range
-  })
+function convertToDestination({ start, end }, destinationBlocks) {
+  const results = []
 
-  if (destination)
-    return seed - destination[1] + destination[0]
+  let current = start
 
-  return seed
+  while (current <= end) {
+    const currentBlock = destinationBlocks.find(([_, source, range]) => source <= current && current < source + range)
+
+    if (currentBlock) {
+      const [destination, source, range] = currentBlock
+      const maxCovered = Math.min(end - source + destination, destination + range - 1)
+      const nextSource = { start: current - source + destination, end: maxCovered }
+      results.push(nextSource)
+      current = Math.min(end + 1, current + nextSource.end - nextSource.start + 1)
+    }
+    else {
+      const nextBlock = destinationBlocks.find(([_, source, range]) => current < source && current + range >= source)
+      if (!nextBlock) {
+        results.push({ start: current, end })
+        current = end + 1
+      }
+      else {
+        results.push({ start: current, end: nextBlock[1] - 1 })
+        current = nextBlock[1]
+      }
+    }
+  }
+
+  return results
 }
 
 export {
