c83f69c2a05244c9d997dd966748486ff2cc741a
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-08 08:28:41 +0100

    8: part two

--- days/8/part-two.js
+++ days/8/part-two.js
@@ -1,6 +1,40 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const { instructions, steps } = input
+  const i = 0
+  const startSteps = steps.filter(step => step.stepName.endsWith('A'))
+  const currSteps = [...startSteps.map(step => step.stepName)]
+
+  return currSteps
+    .map((step) => {
+      return getStepCount(step, steps, instructions)
+    })
+    .reduce(leastCommonMultiple, 1)
+}
+
+function getStepCount(start, steps, instructions) {
+  let i = 0
+  let cur = start
+  while (!cur.endsWith('Z')) {
+    const instruction = instructions.charAt(i % instructions.length)
+    const step = steps.find(step => step.stepName === cur)
+    cur = step[instruction]
+    i++
+  }
+  return i
+}
+
+function leastCommonMultiple(a, b) {
+  return (a / greatestCommonDivisor(a, b)) * b
+}
+
+function greatestCommonDivisor(a, b) {
+  if (b === 0)
+    return a
+  else
+    return greatestCommonDivisor(b, a % b)
+}
 
 export {
   getResult,
--- days/8/part-two.test.js
+++ days/8/part-two.test.js
@@ -5,12 +5,21 @@ import { parseInputString } from './input.js'
 describe('day8 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(6)
     })
   })
 })
