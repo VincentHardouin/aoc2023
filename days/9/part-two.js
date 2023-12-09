2656d15a834287d8d39589ac534f3f7d1c187879
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-09 13:05:42 +0100

    9: part two with part one and reverse line

--- days/9/part-two.js
+++ days/9/part-two.js
@@ -2,11 +2,13 @@ import { getInput } from './input.js'
 
 function getResult(input = getInput()) {
   return input
+    .map(line => line.reverse())
     .map(getLineNextValue)
     .reduce((acc, cur) => acc + cur, 0)
 }
 
 function getLineNextValue(line) {
+  const lines = [line]
   while (lastLineIsNotFullOfZeroes(lines)) {
     const lastLine = lines.at(-1)
     const nextLine = []
@@ -15,8 +17,9 @@ function getLineNextValue(line) {
 
     lines.push(nextLine)
   }
+
   const newLines = createNextValue(lines)
-  const newValue = newLines[0].at(0)
+  const newValue = newLines[0].at(-1)
   return newValue
 }
 
@@ -28,13 +31,13 @@ function createNextValue(lines) {
   const newLines = [...lines]
   for (let i = newLines.length - 1; i >= 0; i--) {
     if (i === newLines.length - 1) {
-      newLines[i] = [0, ...newLines[i]]
+      newLines[i].push(0)
     }
     else {
-      const lastNumber = newLines[i].at(0)
-      const previousLastLineNumber = newLines[i + 1].at(0)
-      const nextNumber = lastNumber - previousLastLineNumber
-      newLines[i] = [nextNumber, ...newLines[i]]
+      const lastNumber = newLines[i].at(-1)
+      const previousLastLineNumber = newLines[i + 1].at(-1)
+      const nextNumber = lastNumber + previousLastLineNumber
+      newLines[i].push(nextNumber)
     }
   }
   return newLines
