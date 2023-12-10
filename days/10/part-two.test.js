c2b90cb27adfd7efb5dd44d0be651b302d8ee1f9
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-10 22:16:16 +0100

    10: part two

--- days/10/part-two.test.js
+++ days/10/part-two.test.js
@@ -5,12 +5,21 @@ import { parseInputString } from './input.js'
 describe('day10 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(8)
     })
   })
 })
