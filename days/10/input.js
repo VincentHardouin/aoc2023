36f2ff3608f5352781fcfd71625439a79c620d92
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-10 17:09:07 +0100

    10: generate

---
+++ days/10/input.js
@@ -0,0 +1,22 @@
+import { join } from 'node:path'
+import { readFileSync } from 'node:fs'
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+function getInput() {
+  return parseInputString(readFileSync(join(__dirname, 'input.txt'), 'utf8').toString())
+}
+
+function parseInputString(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map((value) => {
+      return value
+    })
+}
+
+export {
+  getInput,
+  parseInputString,
+}
