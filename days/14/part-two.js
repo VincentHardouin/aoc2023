fdb2d1a59e0f32921cedc17388ef7695ea3b3c91
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-14 11:29:54 +0100

    14: part two

--- days/14/part-two.js
+++ days/14/part-two.js
@@ -1,7 +1,110 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+const gridCache = new Map()
+
+function getResult(input = getInput()) {
+  let tilted = input
+  for (let i = 0; i < 1_000_000_000; i++) {
+    tilted = spinCycle(tilted)
+    const gridKey = tilted.map(l => l.join()).join('\n')
+    if (gridCache.has(gridKey)) {
+      const cycleLength = i - gridCache.get(gridKey)
+      const cyclesLeft = Math.floor((1_000_000_000 - i) / cycleLength)
+      i += cyclesLeft * cycleLength
+    }
+    else {
+      gridCache.set(gridKey, i)
+    }
+  }
+
+  return tilted
+    .map(row => row.filter(c => c === 'O').length)
+    .reduce((acc, row, i) => acc + (tilted.length - i) * row, 0)
+}
+
+const lineCache = new Map()
+
+const tiltNorthCache = new Map()
+
+function tiltNorth(grid) {
+  const gridKey = grid.map(l => l.join()).join('\n')
+  if (tiltNorthCache.has(gridKey))
+    return tiltNorthCache.get(gridKey)
+
+  const g = transpose(grid)
+
+  for (let i = 0; i < g.length; i++) {
+    if (lineCache.has(g[i].join())) {
+      lineCache.get(g[i].join())
+      continue
+    }
+
+    for (let j = 1; j < g[i].length; j++) {
+      if (g[i][j] === 'O') {
+        for (let k = j - 1; k >= 0; k--) {
+          if (g[i][k] === 'O' || g[i][k] === '#') {
+            g[i][j] = '.'
+            g[i][k + 1] = 'O'
+            break
+          }
+          if (k === 0) {
+            g[i][k] = 'O'
+            g[i][j] = '.'
+          }
+        }
+      }
+    }
+
+    lineCache.set(g[i].join(), g[i])
+  }
+
+  const transposeGrid = transpose(g)
+  tiltNorthCache.set(gridKey, transposeGrid)
+  return transposeGrid
+}
+
+function tiltWest(grid) {
+  const gr = rotateClockwise(grid)
+  const r = tiltNorth(gr)
+  return rotateCounterClockwise(r)
+}
+
+function tiltEast(grid) {
+  const gr = rotateCounterClockwise(grid)
+  const r = tiltNorth(gr)
+  return rotateClockwise(r)
+}
+
+function tiltSouth(grid) {
+  const gr = rotateCounterClockwise(grid)
+  const gr2 = rotateCounterClockwise(gr)
+
+  const r = tiltNorth(gr2)
+
+  return rotateClockwise(rotateClockwise(r))
+}
+
+function spinCycle(grid) {
+  return tiltEast(tiltSouth(tiltWest(tiltNorth(grid))))
+}
+
+function transpose(array) {
+  return array[0].map((_, colIndex) => array.map(row => row[colIndex]))
+}
+
+function rotateClockwise(matrix) {
+  return matrix[0].map((val, index) => matrix.map(row => row[index]).reverse())
+}
+
+function rotateCounterClockwise(matrix) {
+  return matrix[0].map((val, index) => matrix.map(row => row[row.length - 1 - index]))
+}
 
 export {
   getResult,
+  tiltNorth,
+  tiltWest,
+  tiltEast,
+  tiltSouth,
+  spinCycle,
 }
--- days/14/part-two.test.js
+++ days/14/part-two.test.js
@@ -5,12 +5,171 @@ import { parseInputString } from './input.js'
 describe('day14 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(64)
+    })
+  })
+
+  describe('#tiltWest', () => {
+    it('should return grid with rocks slide in the north', () => {
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+      const input = parseInputString(inputString)
+
+      const result = partTwo.tiltWest(input)
+
+      const expectedResult = parseInputString(`O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....`)
+      expect(result).toStrictEqual(expectedResult)
+    })
+  })
+
+  describe('#tiltEast', () => {
+    it('should return grid with rocks slide in the north', () => {
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+      const input = parseInputString(inputString)
+
+      const result = partTwo.tiltEast(input)
+
+      const expectedResult = parseInputString(`....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....`)
+      expect(result).toStrictEqual(expectedResult)
+    })
+  })
+
+  describe('#tiltSouth', () => {
+    it('should return grid with rocks slide in the south', () => {
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+      const input = parseInputString(inputString)
+
+      const result = partTwo.tiltSouth(input)
+
+      const expectedResult = parseInputString(`.....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O`)
+      expect(result).toStrictEqual(expectedResult)
+    })
+  })
+
+  describe('#spinCycle', () => {
+    it('should return grid with spin', () => {
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+      const input = parseInputString(inputString)
+
+      const result = partTwo.spinCycle(input)
+
+      const expectedResult = parseInputString(`.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....`)
+      expect(result).toStrictEqual(expectedResult)
+    })
+
+    it('should return after 3 cycle', () => {
+      const inputString = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+      const input = parseInputString(inputString)
+
+      const result = partTwo.spinCycle(input)
+      const result1 = partTwo.spinCycle(result)
+      const result2 = partTwo.spinCycle(result1)
+
+      const expectedResult = parseInputString(`.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O`)
+      expect(result2).toStrictEqual(expectedResult)
     })
   })
 })
