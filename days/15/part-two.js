f7f1e616318a0ce85eb827e82d289e274fd1f08d
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-15 10:31:44 +0100

    15: part two map

--- days/15/part-two.js
+++ days/15/part-two.js
@@ -1,6 +1,46 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const boxes = new Map()
+
+  input.forEach((step) => {
+    const [label, operator, focalLength] = step.split(/([=-])/)
+
+    const hashedLabel = getHash(label)
+
+    let box
+    if (boxes.has(hashedLabel))
+      box = boxes.get(hashedLabel)
+    else
+      box = boxes.set(hashedLabel, []).get(hashedLabel)
+
+    const i = box.findIndex(l => l.label === label)
+    if (operator === '-' && i !== -1) {
+      box.splice(i, 1)
+    }
+    else if (operator === '=') {
+      if (i === -1)
+        box.push({ label, focalLength })
+      else
+        box[i].focalLength = focalLength
+    }
+  })
+
+  return [...boxes]
+    .map(([hash, box]) => {
+      return box
+        .map((l, j) => l.focalLength * (j + 1) * (hash + 1))
+        .reduce((acc, curr) => acc + curr, 0)
+    }).reduce((acc, curr) => acc + curr, 0)
+}
+
+function getHash(str) {
+  let hash = 0
+  for (const c of str)
+    hash = ((hash + c.charCodeAt(0)) * 17) % 256
+
+  return hash
+}
 
 export {
   getResult,
--- days/15/part-two.test.js
+++ days/15/part-two.test.js
@@ -5,12 +5,12 @@ import { parseInputString } from './input.js'
 describe('day15 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(145)
     })
   })
 })
