1742d6c6b1d120900626906be22aa3da71f87858
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-21 08:44:43 +0100

    20: part one

--- days/20/part-one.js
+++ days/20/part-one.js
@@ -1,6 +1,67 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const modules = input
+    .map((module) => {
+      if (module.type === '%')
+        module.state = false
+      if (module.type === '&')
+        module.history = []
+
+      return module
+    })
+
+  const buttonPush = modules
+    .find(mod => mod.type === 'b')
+
+  const queue = []
+
+  let buttonPushes = 0
+  const stats = {
+    low: 0,
+    high: 0,
+  }
+  while (queue.length > 0 || buttonPushes < 1) {
+    if (queue.length === 0) {
+      buttonPushes++
+      buttonPush.destinations.forEach((dest) => {
+        queue.push({ from: 'broadcaster', target: dest, pulseType: 'low' })
+      })
+      stats.low++
+    }
+
+    const next = queue.shift()
+    console.log(next)
+    const module = modules.find(mod => mod.moduleName === next.target)
+    if (!module)
+      continue
+
+    stats[next.pulseType]++
+
+    if (module.type === 'b') {
+      module.destinations.forEach((dest) => {
+        queue.push({ from: module.moduleName, target: dest, pulseType: 'low' })
+      })
+    }
+    else if (module.type === '%' && next.pulseType === 'low') {
+      module.state = !module.state
+      const pulse = module.state ? 'high' : 'low'
+      module.destinations.forEach((dest) => {
+        queue.push({ from: module.moduleName, target: dest, pulseType: pulse })
+      })
+    }
+    else if (module.type === '&') {
+      module.history[next.from] = next.pulseType
+      const pulse = Object.values(module.history).every(pulse => pulse === 'high') ? 'low' : 'high'
+      module.destinations.forEach((dest) => {
+        queue.push({ from: module.moduleName, target: dest, pulseType: pulse })
+      })
+    }
+  }
+  console.log(stats)
+
+  return stats.low * stats.high
+}
 
 export {
   getResult,
--- days/20/part-one.test.js
+++ days/20/part-one.test.js
@@ -4,13 +4,30 @@ import { parseInputString } from './input.js'
 
 describe('day20 | part-one', () => {
   describe('#getResult', () => {
-    it('should ', () => {
-      const inputString = ``
+    it('should return 32000000', () => {
+      const inputString = `broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a`
       const input = parseInputString(inputString)
 
       const result = partOne.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(32000000)
+    })
+
+    it('should return 11687500', () => {
+      const inputString = `broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output`
+      const input = parseInputString(inputString)
+
+      const result = partOne.getResult(input)
+
+      expect(result).toBe(11687500)
     })
   })
 })
