906fae6a611f879d86b68328b32ea3906de6ddea
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-23 22:24:45 +0100

    22: part two

--- days/22/part-two.js
+++ days/22/part-two.js
@@ -1,6 +1,64 @@
 import { getInput } from './input.js'
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const bricks = input
+    .sort((a, b) => a.end[2] - b.end[2])
+
+  fullFall(bricks)
+
+  return bricks.reduce((acc, brick, i) => {
+    const subBricks = JSON.parse(JSON.stringify(bricks))
+    subBricks.splice(i, 1)
+
+    const fallen = fullFall(subBricks)
+    return acc + fallen
+  }, 0)
+}
+
+function fullFall(bricks) {
+  const fallen = new Set()
+  let snapshot = ''
+  while (snapshot !== getSnapshot(bricks)) {
+    snapshot = getSnapshot(bricks)
+    fall(bricks, fallen)
+  }
+
+  return fallen.size
+}
+
+function getSnapshot(bricks) {
+  return bricks.map(({ end }) => end[2]).join(',')
+}
+
+function fall(bricks, fallen) {
+  bricks.forEach((brick, i) => {
+    if (canFall(brick, bricks)) {
+      brick.start[2]--
+      brick.end[2]--
+      fallen.add(i)
+    }
+  })
+}
+
+function canFall(brick, bricks) {
+  const { start, end } = brick
+  const [x1, y1, z1] = start
+  const [x2, y2, z2] = end
+
+  if (z1 <= 1)
+    return false
+
+  const z = z1 - 1
+
+  for (let x = x1; x <= x2; x++) {
+    for (let y = y1; y <= y2; y++) {
+      if (bricks.some(({ start, end }) => end !== brick.end && start[0] <= x && end[0] >= x && start[1] <= y && end[1] >= y && start[2] <= z && end[2] >= z))
+        return false
+    }
+  }
+
+  return true
+}
 
 export {
   getResult,
--- days/22/part-two.test.js
+++ days/22/part-two.test.js
@@ -5,12 +5,18 @@ import { parseInputString } from './input.js'
 describe('day22 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``
+      const inputString = `1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9`
       const input = parseInputString(inputString)
 
       const result = partTwo.getResult(input)
 
-      expect(result).toBe()
+      expect(result).toBe(7)
     })
   })
 })
