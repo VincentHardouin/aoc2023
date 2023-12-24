66fcc332002d26fb07333f7aef3a5f97a91abc65
Vincent Hardouin <vincenthardouin@icloud.com>
2023-12-24 11:09:31 +0100

    24: generate

---
+++ days/24/part-one.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import * as partOne from './part-one.js'
+import { parseInputString } from './input.js'
+
+describe('day24 | part-one', () => {
+  describe('#getResult', () => {
+    it('should ', () => {
+      const inputString = ``
+      const input = parseInputString(inputString)
+
+      const result = partOne.getResult(input)
+
+      expect(result).toBe()
+    })
+  })
+})
